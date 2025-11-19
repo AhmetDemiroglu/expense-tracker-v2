7a0a72bac4774055162b56cb1430993aa8010285
Ahmet Demiroğlu <ahmetdemiroglu89@hotmail.com>
2025-11-19 17:07:27 +0300

    +

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,21 +1,18 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD8tzJ-ByET-_eQD4PyF68uTqLsiaIAhlg",
-  authDomain: "expense-tracker-v2-f5a0b.firebaseapp.com",
-  projectId: "expense-tracker-v2-f5a0b",
-  storageBucket: "expense-tracker-v2-f5a0b.firebasestorage.app",
-  messagingSenderId: "774300681132",
-  appId: "1:774300681132:web:7a76bc298443638c5b3009",
-  measurementId: "G-DW40K8BL6P"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize app
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
