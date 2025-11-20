a6ed70bea4221d7bc7e749c707d4929eaabe6436
Ahmet Demiroğlu <ahmetdemiroglu89@hotmail.com>
2025-11-20 17:14:58 +0300

    +

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{ts,tsx,js,jsx}",
+    "./*.{js,ts,jsx,tsx}",       
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./context/**/*.{js,ts,jsx,tsx}",   
+    "./services/**/*.{js,ts,jsx,tsx}"     
   ],
   theme: {
     extend: {
